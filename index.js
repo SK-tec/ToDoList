60403c4fdbd5b93fe68e402722f6aa87e2363709
samatha <samathagamidi@gmail.com>
2023-08-09 12:30:46 +0200

    Retriving the item with id

--- index.js
+++ index.js
@@ -15,6 +15,29 @@ app.get("/todos", (req, res) => {
     return res.json({ todos: todos });
   });
 });
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const findTodoById = (todos, id) => {
+    for (let index = 0; index < todos.length; index++) {
+      if (todos[index].id === id) {
+        return index;
+      }
+    }
+    return -1;
+  };
+  fs.readFile("./store/todolist.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("Some thing went wrong in Reading file");
+    }
+    let todos = JSON.parse(data);
+    const todoIndex = findTodoById(todos, id);
+    if (todoIndex === -1) {
+      return res.status(404).send("Item with the given Id not found");
+    }
+    return res.json(todos[todoIndex]);
+  });
+});
 app.listen(port, () => {
   console.log(`Application is running on the port ${port}`);
 });
